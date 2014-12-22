762f1b22baa19a52764bdfb907f828edf045e1f4
Breyten Ernsting <bje@dds.nl>
2014-12-22 16:10:19 +0100

    Initial commit -- just standard filler stuff.

---
+++ usrefs.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: US Amsterdam Referee System
+* Plugin URI: http://www.usvolleybal.nl/competitie/scheidsrechters-leveren/
+* Description: Allows members from US to assign themselves to a referee match
+* Version: 0.0.1
+* Author: Breyten Ernsting
+* Author URI: http://yerb.net/
+* License: MIT
+*/
